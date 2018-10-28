a9b8c4ddd86a7052cb92344ee17c285f08e86b76
Adam Jędrych <adam.jedrych25@gmail.com>
2018-10-28 13:49:14 +0100

    Add reduce sum gradient tests

--- tests/reduceSumLayerTests.cpp
+++ tests/reduceSumLayerTests.cpp
@@ -53,9 +53,44 @@ class ReduceSumTest : public LayerTest,
         bool correct = runTest({input}, {output}, builder);
         EXPECT_TRUE(correct);
     }
+
+    void testGradient(const TestCase& testCase)
+    {
+        UniformGen gen(0);
+        RefTensor input(testCase);
+        RefTensor outputGrad(TensorShape({}));
+        RefTensor inputGrad(testCase);
+        input.fillRandomly(gen);
+        outputGrad.fillRandomly(gen);
+
+        for (std::size_t pos = 0; pos < input.count(); ++pos)
+            inputGrad.at(pos) = outputGrad.at(0);
+
+        LayerBuilder builder = [&testCase](const HostVec& ins,
+                                           const HostVec& outs) {
+            Tensor::SPtr in = core::getDefaultGraph()->addInput("in", testCase);
+            Tensor::SPtr outG = core::getDefaultGraph()->addInput("outG", {});
+            Tensor::SPtr out = core::reduceSum(in);
+            Oper::SPtr oper =
+                std::make_shared<ReduceSumGradientOper>(in, out, outG);
+            core::getDefaultGraph()->insertOperation(oper);
+            Tensor::SPtr inG = oper->getOutputs()[0];
+            initializeGraph();
+
+            inG->eval({{"in", ins[0]}, {"outG", ins[1]}}, outs[0]);
+        };
+        bool correct = runTest({input, outputGrad}, {inputGrad}, builder);
+        EXPECT_TRUE(correct);
+    }
 };
 
 TEST_P(ReduceSumTest, testAPI) { test(GetParam()); }
 INSTANTIATE_TEST_CASE_P(LayerTest, ReduceSumTest, ValuesIn(SHAPES));
+
+class ReduceSumGradientTest : public ReduceSumTest
+{
+};
+TEST_P(ReduceSumGradientTest, testAPI) { testGradient(GetParam()); }
+INSTANTIATE_TEST_CASE_P(LayerTest, ReduceSumGradientTest, ValuesIn(SHAPES));
 
 }  // namespace
