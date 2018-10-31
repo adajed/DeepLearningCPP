9e21f4cff65576886d20bd65486817349affa2c9
Adam Jędrych <adam.jedrych25@gmail.com>
2018-10-31 13:32:32 +0100

    Move addN to new API

--- core/layers/addN.cpp
+++ core/layers/addN.cpp
@@ -1,38 +1,33 @@
 #include "addN.h"
-#include "dll_ops.h"
+#include <assert.h>
+#include "abstractTensor.h"
 #include "graph.h"
+#include "graphdl_ops.h"
 
-namespace dll
+namespace graphdl
 {
 namespace core
 {
 namespace layers
 {
-std::vector<Tensor::SPtr> createOutput(std::vector<Tensor::SPtr> ins)
-{
-    assert(ins.size() > 0);
-    for (unsigned i = 1; i < ins.size(); ++i)
-        assert(ins[0]->shape() == ins[i]->shape());
-    return {createTensor("", ins[0]->shape())};
-}
-
-AddNOper::AddNOper(std::vector<Tensor::SPtr> tensors)
-    : GradientOper(tensors, createOutput(tensors))
+AddNLayer::AddNLayer(ID id, std::vector<Tensor::SPtr> tensors)
+    : DifferentiableLayer(id, tensors,
+                          {createTensor("", tensors[0]->getShape())})
 {
 }
 
-void AddNOper::executeOper(const InputDict& inputs)
+void AddNLayer::execute(const InputDict& inputs)
 {
     std::vector<Tensor::SPtr> ins = getInputs();
     std::vector<Memory> inMemory;
     for (Tensor::SPtr in : ins)
     {
-        in->exec(inputs);
+        in->eval(inputs);
         inMemory.push_back(in->getMemory());
     }
     Memory outMemory = mOutputs[0]->getMemory();
 
-    for (std::size_t pos = 0; pos < outMemory.count(); ++pos)
+    for (std::size_t pos = 0; pos < outMemory.getCount(); ++pos)
     {
         outMemory[pos] = 0.;
         for (unsigned i = 0; i < inMemory.size(); ++i)
@@ -40,17 +35,15 @@ void AddNOper::executeOper(const InputDict& inputs)
     }
 }
 
-GradientOper::TensorMap AddNOper::gradients(Tensor::SPtr out,
-                                            Tensor::SPtr outGrad)
+Layer::TensorMap AddNLayer::gradients(Tensor::SPtr out, Tensor::SPtr outGrad)
 {
     assert(out == mOutputs[0]);
 
     std::vector<Tensor::SPtr> inputs = getInputs();
-    Oper::SPtr oper = std::make_shared<AddNGradientOper>(inputs, out, outGrad);
-    core::getDefaultGraph()->insertOperation(oper);
-    std::vector<Tensor::SPtr> grads = oper->getOutputs();
+    Layer::SPtr layer = createLayer<AddNGradientLayer>(inputs, out, outGrad);
 
-    GradientOper::TensorMap gradMap;
+    std::vector<Tensor::SPtr> grads = layer->getOutputs();
+    Layer::TensorMap gradMap;
     for (unsigned i = 0; i < inputs.size(); ++i)
         gradMap.insert({inputs[i], grads[i]});
     return gradMap;
@@ -67,32 +60,29 @@ std::vector<Tensor::SPtr> createGradientInputs(std::vector<Tensor::SPtr> ins,
 
 std::vector<Tensor::SPtr> createGradientOutputs(std::vector<Tensor::SPtr> ins)
 {
-    assert(ins.size() > 0);
-    for (unsigned i = 1; i < ins.size(); ++i)
-        assert(ins[0]->shape() == ins[i]->shape());
-
     std::vector<Tensor::SPtr> outs;
-    for (Tensor::SPtr i : ins) outs.push_back(createTensor("", i->shape()));
+    for (Tensor::SPtr i : ins) outs.push_back(createTensor("", i->getShape()));
     return outs;
 }
 
-AddNGradientOper::AddNGradientOper(std::vector<Tensor::SPtr> ins,
-                                   Tensor::SPtr out, Tensor::SPtr outGrad)
-    : Oper(createGradientInputs(ins, out, outGrad), createGradientOutputs(ins))
+AddNGradientLayer::AddNGradientLayer(ID id, std::vector<Tensor::SPtr> ins,
+                                     Tensor::SPtr out, Tensor::SPtr outGrad)
+    : Layer(id, createGradientInputs(ins, out, outGrad),
+            createGradientOutputs(ins))
 {
 }
 
-void AddNGradientOper::executeOper(const InputDict& inputs)
+void AddNGradientLayer::execute(const InputDict& inputs)
 {
     Tensor::SPtr outputGrad = mInputs.back().lock();
-    outputGrad->exec(inputs);
+    outputGrad->eval(inputs);
 
     Memory outG = outputGrad->getMemory();
     std::vector<Memory> inG;
     for (unsigned i = 0; i < mOutputs.size(); ++i)
         inG.push_back(mOutputs[i]->getMemory());
 
-    for (std::size_t pos = 0; pos < outG.count(); ++pos)
+    for (std::size_t pos = 0; pos < outG.getCount(); ++pos)
     {
         for (unsigned i = 0; i < inG.size(); ++i) inG[i][pos] = outG[pos];
     }
@@ -103,28 +93,26 @@ void AddNGradientOper::executeOper(const InputDict& inputs)
 Tensor::SPtr addN(std::vector<Tensor::SPtr> tensors)
 {
     if (tensors.size() == 0)
-        throw std::invalid_argument("List of input tensors cannot be empty");
+        throw std::runtime_error("List of input tensors cannot be empty");
     for (unsigned i = 1; i < tensors.size(); ++i)
     {
-        if (tensors[0]->shape() != tensors[i]->shape())
-            throw std::invalid_argument(
-                "Shapes of inputs tensors don\'t match");
+        if (tensors[0]->getShape() != tensors[i]->getShape())
+            throw std::runtime_error("Shapes of inputs tensors don\'t match");
     }
 
-    Oper::SPtr oper = std::make_shared<layers::AddNOper>(tensors);
-    core::getDefaultGraph()->insertOperation(oper);
-    return oper->getOutputs()[0];
+    Layer::SPtr layer = createLayer<layers::AddNLayer>(tensors);
+    return layer->getOutputs()[0];
 }
 
 }  // namespace core
 
-ITensorSPtr addN(std::vector<ITensorSPtr> tensors)
+ITensorPtr addN(std::vector<ITensorPtr> tensors)
 {
     std::vector<core::Tensor::SPtr> inputs;
-    for (ITensorSPtr t : tensors)
-        inputs.push_back(std::static_pointer_cast<core::Tensor>(t));
+    for (ITensorPtr t : tensors)
+        inputs.push_back(core::castITensorPtr(t)->get());
 
-    return ITensorSPtr(core::addN(inputs));
+    return makeAbstractTensor(core::addN(inputs));
 }
 
-}  // namespace dll
+}  // namespace graphdl
