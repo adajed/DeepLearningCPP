8275caeb1c31a0cbd3d2915ea1a01968818df1b2
Adam Jędrych <adam.jedrych25@gmail.com>
2018-10-31 13:02:08 +0100

    Move assign to new API

--- core/layers/assign.cpp
+++ core/layers/assign.cpp
@@ -1,50 +1,50 @@
 #include "assign.h"
 #include <assert.h>
-#include "dll_errors.h"
-#include "dll_ops.h"
+#include "abstractTensor.h"
 #include "graph.h"
+#include "graphdl_ops.h"
 
-namespace dll
+namespace graphdl
 {
 namespace core
 {
 namespace layers
 {
-AssignOper::AssignOper(Tensor::SPtr dest, Tensor::SPtr src)
-    : Oper({src}, {createTensor("", {0})}), mDest(dest)
+AssignLayer::AssignLayer(ID id, Tensor::SPtr dest, Tensor::SPtr src)
+    : Layer(id, {src}, {createTensor("", {0})}), mDest(dest)
 {
-    assert(dest->shape() == src->shape());
+    assert(dest->getShape() == src->getShape());
 }
 
-void AssignOper::executeOper(const InputDict& inputs)
+void AssignLayer::execute(const InputDict& inputs)
 {
     Tensor::SPtr src = mInputs[0].lock();
     Tensor::SPtr dest = mDest.lock();
-    src->exec(inputs);
+    src->eval(inputs);
 
     Memory in = src->getMemory();
     Memory out = dest->getMemory();
-    for (std::size_t pos = 0; pos < in.count(); ++pos) out[pos] = in[pos];
+    for (std::size_t pos = 0; pos < in.getCount(); ++pos) out[pos] = in[pos];
 }
 
 }  // namespace layers
 
 Tensor::SPtr assign(Tensor::SPtr dest, Tensor::SPtr src)
 {
-    if (dest->shape() != src->shape()) throw errors::NotMatchingShapesError();
+    if (dest->getShape() != src->getShape())
+        throw std::runtime_error("Shapes don\'t match");
 
-    Oper::SPtr oper = std::make_shared<layers::AssignOper>(dest, src);
-    getDefaultGraph()->insertOperation(oper);
-    return oper->getOutputs()[0];
+    Layer::SPtr layer = createLayer<layers::AssignLayer>(dest, src);
+    return layer->getOutputs()[0];
 }
 
 }  // namespace core
 
-ITensorSPtr assign(ITensorSPtr dest, ITensorSPtr src)
+ITensorPtr assign(ITensorPtr dest, ITensorPtr src)
 {
-    core::Tensor::SPtr destT = std::static_pointer_cast<core::Tensor>(dest);
-    core::Tensor::SPtr srcT = std::static_pointer_cast<core::Tensor>(src);
-    return ITensorSPtr(core::assign(destT, srcT));
+    core::AbstractTensor::Ptr aDest = core::castITensorPtr(dest);
+    core::AbstractTensor::Ptr aSrc = core::castITensorPtr(src);
+    return makeAbstractTensor(core::assign(aDest->get(), aSrc->get()));
 }
 
-}  // namespace dll
+}  // namespace graphdl
