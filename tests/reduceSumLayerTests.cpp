21eb3225087bff062a73252b6e3d083cf108d643
Adam Jędrych <adam.jedrych25@gmail.com>
2018-10-31 20:15:40 +0100

    Move reduce sum test to new API

--- tests/reduceSumLayerTests.cpp
+++ tests/reduceSumLayerTests.cpp
@@ -1,4 +1,5 @@
-#include "dll_ops.h"
+#include "abstractTensor.h"
+#include "graphdl_ops.h"
 #include "layerTests.h"
 #include "reduceSum.h"
 
@@ -39,16 +40,15 @@ class ReduceSumTest : public LayerTest,
         input.fillRandomly(gen);
 
         output.at(0) = 0.;
-        for (std::size_t pos = 0; pos < input.count(); ++pos)
+        for (std::size_t pos = 0; pos < input.getCount(); ++pos)
             output.at(0) += input.at(pos);
 
-        LayerBuilder builder = [&testCase](const HostVec& ins,
-                                           const HostVec& outs) {
+        LayerBuilder builder = [&testCase](const HostVec& ins) {
             ITensorPtr in = createInput("in", testCase);
             ITensorPtr out = reduceSum(in);
             initializeGraph();
 
-            out->eval({{"in", ins[0]}}, outs[0]);
+            return HostVec({out->eval({{"in", ins[0]}})});
         };
         bool correct = runTest({input}, {output}, builder);
         EXPECT_TRUE(correct);
@@ -63,21 +63,22 @@ class ReduceSumTest : public LayerTest,
         input.fillRandomly(gen);
         outputGrad.fillRandomly(gen);
 
-        for (std::size_t pos = 0; pos < input.count(); ++pos)
+        for (std::size_t pos = 0; pos < input.getCount(); ++pos)
             inputGrad.at(pos) = outputGrad.at(0);
 
-        LayerBuilder builder = [&testCase](const HostVec& ins,
-                                           const HostVec& outs) {
-            Tensor::SPtr in = core::getDefaultGraph()->addInput("in", testCase);
-            Tensor::SPtr outG = core::getDefaultGraph()->addInput("outG", {});
+        LayerBuilder builder = [&testCase](const HostVec& ins) {
+            Tensor::SPtr in = core::getDefaultGraph()->addInput(
+                "in", createLayer<InputLayer>("in", testCase));
+            Tensor::SPtr outG = core::getDefaultGraph()->addInput(
+                "outG", createLayer<InputLayer>("outG", Shape({})));
             Tensor::SPtr out = core::reduceSum(in);
-            Oper::SPtr oper =
-                std::make_shared<ReduceSumGradientOper>(in, out, outG);
-            core::getDefaultGraph()->insertOperation(oper);
-            Tensor::SPtr inG = oper->getOutputs()[0];
+            Layer::SPtr layer =
+                createLayer<ReduceSumGradientLayer>(in, out, outG);
+            Tensor::SPtr inG = layer->getOutputs()[0];
             initializeGraph();
 
-            inG->eval({{"in", ins[0]}, {"outG", ins[1]}}, outs[0]);
+            AbstractTensor::Ptr t = makeAbstractTensor(inG);
+            return HostVec({t->eval({{"in", ins[0]}, {"outG", ins[1]}})});
         };
         bool correct = runTest({input, outputGrad}, {inputGrad}, builder);
         EXPECT_TRUE(correct);
