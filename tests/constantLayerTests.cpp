20481a231e4838078b8766cf8e873758d2bca16e
Adam Jędrych <adam.jedrych25@gmail.com>
2018-11-05 00:19:53 +0100

    Add constant tests

---
+++ tests/constantLayerTests.cpp
@@ -0,0 +1,51 @@
+#include "constant.h"
+#include "graphdl_ops.h"
+#include "layerTests.h"
+
+namespace
+{
+using TestCase = std::tuple<Vec, float, MemoryLocation>;
+
+std::vector<Vec> SHAPES = {
+    // clang-format off
+    {},
+    {1},
+    {1, 1},
+    {1, 1, 1},
+    {1, 1, 1, 1},
+    {1, 1, 1, 1, 1},
+    {2},
+    {2, 2},
+    {2, 2, 2},
+    {2, 2, 2, 2},
+    {2, 2, 2, 2, 2}
+    // clang-format on
+};
+
+class ConstantTest : public LayerTest,
+                     public testing::WithParamInterface<TestCase>
+{
+   public:
+    void test(const TestCase& testCase)
+    {
+        RefTensor tensor(std::get<0>(testCase));
+        for (std::size_t pos = 0; pos < tensor.getCount(); ++pos)
+            tensor.at(pos) = std::get<1>(testCase);
+
+        LayerBuilder builder = [&testCase](const HostVec& ins) {
+            ITensorPtr c = constant(
+                    std::get<1>(testCase), std::get<0>(testCase), std::get<2>(testCase));
+            initializeGraph();
+
+            return HostVec({c->eval({})});
+        };
+        bool correct = runTest({}, {tensor}, builder);
+        EXPECT_TRUE(correct);
+    }
+};
+
+TEST_P(ConstantTest, testAPI) { test(GetParam()); }
+INSTANTIATE_TEST_CASE_P(LayerTest, ConstantTest,
+                        Combine(ValuesIn(SHAPES), ValuesIn({3.14f}), ValuesIn(LOCATIONS)));
+
+}  // namespace
