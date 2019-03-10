aff47eb7036719947e7ad936a59eedf3160e74d4
Adam Jędrych <adam.jedrych25@gmail.com>
2019-03-10 20:55:43 +0100

    Improve matmul gradient on CUDA

--- core/layers/cuda/matmul.cu
+++ core/layers/cuda/matmul.cu
@@ -8,13 +8,14 @@ namespace layers
 {
 namespace cuda
 {
-template <int TILE_SIZE>
+template <int TILE_SIZE, bool tran1, bool tran2>
 __global__ void matmulKernel(int n, int m, int k, float* X1, float* X2,
                              float* Y)
 {
     __shared__ float tile_X1[TILE_SIZE * TILE_SIZE];
     __shared__ float tile_X2[TILE_SIZE * TILE_SIZE];
 
+    int pos = TILE_SIZE * threadIdx.x + threadIdx.y;
     int row = blockDim.x * blockIdx.x + threadIdx.x;
     int col = blockDim.y * blockIdx.y + threadIdx.y;
     float tmp = 0.;
@@ -22,16 +23,24 @@ __global__ void matmulKernel(int n, int m, int k, float* X1, float* X2,
     for (int t = 0; t < m; t += TILE_SIZE)
     {
         if (t + threadIdx.y < m)
-            tile_X1[TILE_SIZE * threadIdx.x + threadIdx.y] =
-                X1[m * row + t + threadIdx.y];
+        {
+            if (tran1)
+                tile_X1[pos] = X1[n * (t + threadIdx.y) + row];
+            else
+                tile_X1[pos] = X1[m * row + t + threadIdx.y];
+        }
         else
-            tile_X1[TILE_SIZE * threadIdx.x + threadIdx.y] = 0.;
+            tile_X1[pos] = 0.;
 
         if (t + threadIdx.x < m)
-            tile_X2[TILE_SIZE * threadIdx.x + threadIdx.y] =
-                X2[k * (t + threadIdx.x) + col];
+        {
+            if (tran2)
+                tile_X2[pos] = X2[m * col + t + threadIdx.x];
+            else
+                tile_X2[pos] = X2[k * (t + threadIdx.x) + col];
+        }
         else
-            tile_X2[TILE_SIZE * threadIdx.x + threadIdx.y] = 0.;
+            tile_X2[pos] = 0.;
 
         __syncthreads();
 
@@ -77,19 +86,24 @@ extern "C" void runMatmulDevice(int n, int m, int k, float* X1, float* X2,
     const int TILE_SIZE = 16;
     dim3 GRID((n + TILE_SIZE - 1) / TILE_SIZE, (k + TILE_SIZE - 1) / TILE_SIZE);
     dim3 BLOCK(TILE_SIZE, TILE_SIZE);
-    matmulKernel<TILE_SIZE><<<GRID, BLOCK>>>(n, m, k, X1, X2, Y);
+    matmulKernel<TILE_SIZE, false, false><<<GRID, BLOCK>>>(n, m, k, X1, X2, Y);
 }
 
 extern "C" void runMatmulGradientDevice(int n, int m, int k, float* X1,
                                         float* X2, float* Ygrad, float* X1grad,
                                         float* X2grad)
 {
-    const int BLOCK_SIZE = 256;
-    const int NUM_BLOCKS = (n * m + m * k + BLOCK_SIZE - 1) / BLOCK_SIZE;
+    const int TILE_SIZE = 16;
+    dim3 BLOCK(TILE_SIZE, TILE_SIZE);
+    dim3 GRID1((n + TILE_SIZE - 1) / TILE_SIZE,
+               (m + TILE_SIZE - 1) / TILE_SIZE);
+    dim3 GRID2((m + TILE_SIZE - 1) / TILE_SIZE,
+               (k + TILE_SIZE - 1) / TILE_SIZE);
 
-    matmulGradientKernel<<<NUM_BLOCKS, BLOCK_SIZE>>>(n, m, k, X1, X2, Ygrad,
-                                                     X1grad, X2grad);
-    cudaDeviceSynchronize();
+    matmulKernel<TILE_SIZE, false, true>
+        <<<GRID1, BLOCK>>>(n, k, m, Ygrad, X2, X1grad);
+    matmulKernel<TILE_SIZE, true, false>
+        <<<GRID2, BLOCK>>>(m, n, k, X1, Ygrad, X2grad);
 }
 
 }  // namespace cuda
